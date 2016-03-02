65e51080ef85baeb5e66b68f87122195f92f9aca
Jared Forsyth <jared@jaredforsyth.com>
2016-03-02 16:16:04 -0700

    Implement swift class generation
    Summary:
    The Swift bridging code consists of 2 generated classes; a ViewController
    (ish) and a PropsManager. The PropsManager is solely responsible for
    serialization/deserialization of the props, and the ViewController initializes
    & updates the ReactNative view, and holds onto the state of the current props
    (the original values, not the serialized ones).
    
    This diff represents the last of the swift codegen code.
    
    Test Plan: TODO (see upcoming diff for example output)
    
    Reviewers: charlie
    
    Subscribers: #android, #ios
    
    Differential Revision: https://phabricator.khanacademy.org/D25657

---
+++ src/swift/prepare-props-manager.js
@@ -0,0 +1,62 @@
+/**
+ * Prepare the bulk of the logic for the PropsManager.
+ *
+ * @flow
+ */
+
+import {swiftValueToJson} from "./json-swift";
+import {asSwiftTypeSignature} from "./signatures";
+import {indentedLines, listValues, capitalize} from "./utils";
+
+import type {
+    ObjectAnnotation,
+    Modules,
+    ConfigTypes,
+} from "../types";
+import type {MarshallingMethods} from "./types";
+
+
+export default (
+    props: ObjectAnnotation,
+    modules: Modules,
+    config: ConfigTypes
+): {
+    // a series of methods like `func setThing(thing)` which serializes the
+    // argument and stores it in the `serialized` Dictionary
+    setters: string,
+    // for value types that get passed back from JS (via a callback), we
+    // generate a `deserializeThing` method
+    deserializeMethods: string,
+    // for value types that are passed to JS, we generate a `serializeThing`
+    // method
+    serializeMethods: string,
+} => {
+    const keys = props.keys;
+    const marshallingMethods: MarshallingMethods = {
+        serialize: {},
+        deserialize: {},
+    };
+    const setters = indentedLines(keys.map(key => {
+        const swiftType = asSwiftTypeSignature(props.attrs[key], config);
+        const toJson = indentedLines([swiftValueToJson(
+            key, props.attrs[key], marshallingMethods, modules, config
+        )]);
+        const capName = capitalize(key);
+        return `public func set${capName}(${key}: ${swiftType}) {
+  serialized["${key}"] = ${toJson}
+}`;
+    }), "\n\n");
+
+    let deserializeMethods = indentedLines(
+        listValues(marshallingMethods.deserialize), "\n\n");
+    let serializeMethods = indentedLines(
+        listValues(marshallingMethods.serialize), "\n\n");
+    if (deserializeMethods) {
+        deserializeMethods = "\n\n  " + deserializeMethods;
+    }
+    if (serializeMethods) {
+        serializeMethods = "\n\n  " + serializeMethods;
+    }
+
+    return {setters, deserializeMethods, serializeMethods};
+};
